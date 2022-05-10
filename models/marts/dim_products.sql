d2d2f23939589c4ce3fc5d8b495cc0609b0d3edc
Praveer Kumar <praveerkumar17@gmail.com>
2022-05-10 09:47:12 +0000

    convert product details to product_prices

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,1 +1,6 @@
-select * from `opendatasets-349618.exchange_rates.product_prices`
+select 
+    Product_ID as name,
+    Order_Date as purchase_date,
+    Price as a_price,
+    currency
+from `opendatasets-349618.exchange_rates.product_details`
